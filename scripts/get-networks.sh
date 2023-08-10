ff6c298fc259d74180e9c6906f0d83d76d946de7
ifnesi <inesi@confluent.io>
2023-08-10 11:02:23 +0100

    Minor

--- scripts/get-networks.sh
+++ scripts/get-networks.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 for CONTAINERNAME in broker-west-1 broker-west-2 broker-west-3 broker-east-1 broker-east-2 broker-east-3
 do
 	IPADDR=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${CONTAINERNAME})
